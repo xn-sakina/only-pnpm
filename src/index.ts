5916675d2c184d3802f49008dfb96886df547589
fz6m <59400654+fz6m@users.noreply.github.com>
2022-05-14 14:23:00 +0800

    chore: remove set `only-allow` prepare script 🍇

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { existsSync } from 'fs'
 
 const FORBIDDEN = 'forbidden, use pnpm'
 const MONOREPO_FILES = [`lerna.json`, 'pnpm-workspace.yaml']
-const ONLY_ALLOW = `only-allow`
+// const ONLY_ALLOW = `only-allow`
 
 export const run = async () => {
   const pwd = process.cwd()
@@ -15,20 +15,26 @@ export const run = async () => {
   const pkg = require(pkgPath)
   let pkgMrm = packageJson(pkg)
   const pnpmV = await geyPnpmVersion()
+
+  /**
+   * @deprecated `only-pnpm` dependence `which-pm-runs` too old
+   * See https://github.com/zkochan/which-pm-runs
+   */
   // 1. set only-allow
-  if (!pkgMrm.get('scripts.preinstall') && isMonorepo({ cwd: pwd })) {
-    const isOnlyAllowInstalled =
-      pkg?.devDependencies?.[ONLY_ALLOW] || pkg?.dependencies?.[ONLY_ALLOW]
-    if (!isOnlyAllowInstalled) {
-      await install({ pkg: ONLY_ALLOW, pnpm: pnpmV })
-      // refresh
-      pkgMrm = packageJson(require(pkgPath))
-    }
-    pkgMrm.setScript('preinstall', 'npx only-allow pnpm')
-  }
+  // if (!pkgMrm.get('scripts.preinstall') && isMonorepo({ cwd: pwd })) {
+  //   const isOnlyAllowInstalled =
+  //     pkg?.devDependencies?.[ONLY_ALLOW] || pkg?.dependencies?.[ONLY_ALLOW]
+  //   if (!isOnlyAllowInstalled) {
+  //     await install({ pkg: ONLY_ALLOW, pnpm: pnpmV })
+  //     // refresh
+  //     pkgMrm = packageJson(require(pkgPath))
+  //   }
+  //   pkgMrm.setScript('preinstall', 'npx only-allow pnpm')
+  // }
+
   // 2. set engines
   const pnpmVersion = `^${pnpmV}`
-  const nodeVersion = `>= ${await getNodeVersion()}`
+  const nodeVersion = `>=${await getNodeMajorVersion()}`
   pkgMrm
     .set('engines.pnpm', pnpmVersion)
     .set('engines.node', nodeVersion)
@@ -49,10 +55,11 @@ async function geyPnpmVersion() {
   return (await execa(`pnpm`, ['--version'])).stdout.trim()
 }
 
-async function getNodeVersion() {
-  return (await execa(`node`, ['--version'])).stdout.trim().slice(1)
+async function getNodeMajorVersion() {
+  const version = (await execa(`node`, ['--version'])).stdout.trim().slice(1)
+  return version.split('.')[0]
 }
 
-function isMonorepo(opts: { cwd: string }) {
-  return MONOREPO_FILES.some((file) => existsSync(join(opts.cwd, file)))
-}
+// function isMonorepo(opts: { cwd: string }) {
+//   return MONOREPO_FILES.some((file) => existsSync(join(opts.cwd, file)))
+// }
